4da44a26ebf3f6ef9471105d85e655bebb907a99
Lim_Junyeop <skystom2@naver.com>
2022-05-27 19:26:56 +0900

    Justpractice02

--- src/main/java/ReBoot/basicpractice.java
+++ src/main/java/ReBoot/basicpractice.java
@@ -1,5 +1,7 @@
 package ReBoot;
 
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.Scanner;
 
         /*System.out.println("Hello BackCoder");
@@ -45,7 +47,7 @@ import java.util.Scanner;
 
 /*
 
-*/
+ *//*
 class memberlogin {
     String loginid;
     int password;
@@ -111,9 +113,243 @@ public class basicpractice {
         Scanner parrot = new Scanner(System.in);
         String whatever = parrot.nextLine();
         System.out.println(whatever);
+*/
 
+
+/*class calcul {
+    public int carc(int a, int b) {
+
+        return a+b;
     }
-}
+
+    public void cacal(int c, int d){
+        System.out.println(c+d);
+    }
+
+
+}*/
+/*
+    class calcul {
+        static int a;
+        int b;
+
+        public static void sum(int a, int b) {
+            System.out.println( a + b);
+        }
+        public void subtract(int a, int b) {
+            System.out.println(a-b);
+        }
+        }*/
+
+        class check {
+        static int a = 1;
+        int b= 2;
+
+        static void sum(int c, int d){
+            System.out.println(c+d);
+        }
+            static void sta_sta() {
+                System.out.println(a);}
+
+            //            static void sta_ins() {
+  //              System.out.println(b);}
+            void ins_sta() {
+                System.out.println(a);}
+            void ins_ins() {
+                System.out.println(b);}
+        }
+
+public class basicpractice{
+    public static void main(String[] args) {
+        //calcul.sum(3, 6 );   - static 필요
+
+
+        check.sum(10, 20);
+        check c1 = new check();
+
+        check.sta_sta();
+        c1.ins_ins();
+        c1.ins_sta();
+
+
+
+
+
+
+
+        //
+    }
+    }
+
+
+
+        /*
+        int a = 3;
+        int d = 5;
+        String b = "12";
+//        a += Integer.parseInt(b);
+         String c = Integer.toString(a);
+        System.out.println(b+c);
+
+        System.out.println(args.length);
+
+        for (String e : args) {
+            System.out.println(e);}
+        */
+/*
+        Scanner parrot = new Scanner(System.in);
+
+        while(parrot.hasNextLine()){
+            System.out.println(parrot.nextLine()+"라고 말했습니다.");}
+        parrot.close();
+*/
+/*
+
+        File idset = new File("outsource.txt");
+        try {
+            Scanner superparrot = new Scanner(idset);
+            while (superparrot.hasNextLine()){
+                System.out.println(superparrot.nextLine() + "라구요?");
+            }
+        } catch (FileNotFoundException e) {
+            System.out.println(e.getMessage());
+        }
+*/
+
+
+
+    /*static class agemachine {
+        String output;public String agg(int init, int limit) {
+            String out = "age = ";
+            for (int a = init; a < limit; a++) {
+
+                out+=a;}
+            return out;
+        }
+    }
+
+
+
+
+
+
+
+    public static void main(String[] args) {
+
+        agemachine c1 = new agemachine();
+
+        System.out.println(c1.agg(13,17));
+*/
+/*
+        calcul c1 = new calcul();
+
+        System.out.println(c1.carc(2, 4)*2);
+
+        int P = c1.carc(3, 3);
+        System.out.println(P*10);
+
+        c1.cacal(3, 5);
+
+*/
+
+
+
+        /*
+        int c = (int) 3.14;
+
+        int d = (int) 3.14;
+
+        String aa = "333";
+        String g = Integer.toString(c);
+
+        int ff = Integer.parseInt(aa);
+        System.out.println(ff + 222);
+
+
+        String pp = "First hint : Circle\n";
+        String jf = "Second hint : Time\n";
+        String pa = "Final hint : TicTok\n";
+
+        String hints = pp + jf + pa;
+        System.out.println(hints);
+
+        String qpa = "June";
+        String pma = new String("June");
+        System.out.println(qpa.equals(pma));
+*/
+
+/*
+
+        switch (4) {
+
+            case 1:
+                System.out.println(1);
+                break;
+            case 2:
+                System.out.println(2);
+                break;
+            case 3:
+                System.out.println(3);
+                break;
+            default:
+                System.out.println("목록에 없습니다.");
+        }
+*/
+
+/*
+        int init = 1; int limit = 10;
+        for (int a= init; a<=limit; a++){
+            if (a==5){continue;}
+            System.out.println("coding everybody"+a);
+            if (a==9){break;}
+
+        }
+*/
+
+        /*String[] memeList = new String[4];
+        memeList[0] = "June";
+        memeList[1] = "Tom";
+        memeList[2] = "Luna";
+        memeList[3] = "Teraa";
+
+
+        int memberid = 0;
+
+        for (int b = 0; b<memeList.length; b++){
+            System.out.println(memeList[b]);
+        }
+
+        for (String mem : memeList
+             ) {
+            System.out.println(mem);
+
+        }
+
+
+        int[] age = new int[3];
+        age[0] = 13;
+        age[1] = 22;
+        age[2] = 33;
+
+        for (int agelist: age
+             ) {
+            System.out.println(agelist); }
+
+        */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 /*
 public class basicpractice{
